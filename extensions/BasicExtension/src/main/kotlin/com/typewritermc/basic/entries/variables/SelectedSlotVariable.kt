d1636f70a220e0dc72a2b37f11cbddeb62d5c2c5
Gabber235 <gabber200@icloud.com>
2025-02-01 12:05:40 +0100

    Create SelectedSlotVariable

---
+++ extensions/BasicExtension/src/main/kotlin/com/typewritermc/basic/entries/variables/SelectedSlotVariable.kt
@@ -0,0 +1,30 @@
+package com.typewritermc.basic.entries.variables
+
+import com.typewritermc.core.books.pages.Colors
+import com.typewritermc.core.extension.annotations.Entry
+import com.typewritermc.core.extension.annotations.GenericConstraint
+import com.typewritermc.engine.paper.entry.entries.VarContext
+import com.typewritermc.engine.paper.entry.entries.VariableEntry
+import kotlin.reflect.cast
+
+@Entry(
+    "selected_slot_variable",
+    "The slot the player has selected in their hotbar",
+    Colors.GREEN,
+    "qlementine-icons:empty-slot-16"
+)
+@GenericConstraint(Int::class)
+/**
+ * The `Selected Slot Variable` entry is a variable that returns the slot the player has selected in their hotbar.
+ *
+ * ## How could this be used?
+ * This could be use to replace the player's current item with another item.
+ */
+class SelectedSlotVariable(
+    override val id: String = "",
+    override val name: String = "",
+) : VariableEntry {
+    override fun <T : Any> get(context: VarContext<T>): T {
+        return context.klass.cast(context.player.inventory.heldItemSlot)
+    }
+}
