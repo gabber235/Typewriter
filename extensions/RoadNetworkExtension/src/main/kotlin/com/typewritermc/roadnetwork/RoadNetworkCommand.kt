55266cdb4b52d817269a58dc59464273a8ac91b2
Gabber235 <gabber200@icloud.com>
2025-03-18 17:35:55 +0100

    Add Roadnetwork Info Command

--- extensions/RoadNetworkExtension/src/main/kotlin/com/typewritermc/roadnetwork/RoadNetworkCommand.kt
+++ extensions/RoadNetworkExtension/src/main/kotlin/com/typewritermc/roadnetwork/RoadNetworkCommand.kt
@@ -1,11 +1,14 @@
 package com.typewritermc.roadnetwork
 
+import com.typewritermc.core.entries.ref
 import com.typewritermc.core.extension.annotations.TypewriterCommand
 import com.typewritermc.core.interaction.context
 import com.typewritermc.engine.paper.command.dsl.*
 import com.typewritermc.engine.paper.content.ContentContext
 import com.typewritermc.engine.paper.content.ContentModeTrigger
 import com.typewritermc.engine.paper.entry.triggerFor
+import com.typewritermc.engine.paper.utils.ThreadType
+import com.typewritermc.engine.paper.utils.asMini
 import com.typewritermc.engine.paper.utils.msg
 import com.typewritermc.roadnetwork.content.RoadNetworkContentMode
 import com.typewritermc.roadnetwork.pathfinding.InstanceSpaceCache
@@ -23,6 +26,31 @@ fun CommandTree.roadNetworkCommand() = literal("roadNetwork") {
                     context,
                     RoadNetworkContentMode(context, target)
                 ).triggerFor(target, context())
+            }
+        }
+    }
+
+    literal("info") {
+        withPermission("typewriter.roadNetwork.info")
+        entry<RoadNetworkEntry>("network") { entry ->
+            executePlayerOrTarget { target ->
+                val networkManager = KoinJavaComponent.get<RoadNetworkManager>(RoadNetworkManager::class.java)
+                target.sendActionBar("Loading network...".asMini())
+                ThreadType.DISPATCHERS_ASYNC.launch {
+                    val network = networkManager.getNetwork(entry().ref())
+                    target.sendMessage("""
+                        |<gray><st>${" ".repeat(60)}</st>
+                        |
+                        |<red><b>Road Network Info</b>: <white>${entry().name}
+                        |
+                        |  <gray> - <blue>Nodes: <white>${network.nodes.size}
+                        |  <gray> - <green>Edges: <white>${network.edges.size}
+                        |  <gray> - <dark_gray>Negative Nodes: <white>${network.negativeNodes.size}
+                        |  <gray> - <gold>Modifications: <white>${network.modifications.size}
+                        |  
+                        |<gray><st>${" ".repeat(60)}</st>
+                    """.trimMargin().asMini())
+                }
             }
         }
     }
