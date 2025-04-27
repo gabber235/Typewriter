0d92bc07f2f1be84cdf7dd523d1adb0f44f31079
Gabber235 <gabber200@icloud.com>
2025-04-27 06:41:03 +0200

    Finalize surrealdb provider

--- backend/organization/organizations/src/lib.rs
+++ backend/organization/organizations/src/lib.rs
@@ -1,13 +1,31 @@
+use serde::Deserialize;
+use surrealdb_component::query;
 use wasmcloud_component::http;
 
 struct Component;
 
 http::export!(Component);
 
+#[derive(Debug, Deserialize)]
+struct User {
+    name: String,
+}
+
 impl http::Server for Component {
     fn handle(
         _request: http::IncomingRequest,
     ) -> http::Result<http::Response<impl http::OutgoingBody>> {
+        let result = query("UPSERT users:test SET name = 'test';")
+            .execute()
+            .map_err(|e| http::ErrorCode::InternalError(Some(e.to_string())))?;
+
+        for i in 0..result.len() {
+            let data: Option<User> = result
+                .take(i)
+                .map_err(|e| http::ErrorCode::InternalError(Some(e.to_string())))?;
+            println!("{:?}", data);
+        }
+
         Ok(http::Response::new("Hello from Rust!\n"))
     }
 }
