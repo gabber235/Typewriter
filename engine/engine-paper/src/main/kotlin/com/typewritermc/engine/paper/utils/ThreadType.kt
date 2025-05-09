16924fdae5aa237b482b44b9b90e06373eae8244
Gabber235 <gabber200@icloud.com>
2025-05-09 08:24:47 +0200

    Make ThreadType.ASYNC actually run async

--- engine/engine-paper/src/main/kotlin/com/typewritermc/engine/paper/utils/ThreadType.kt
+++ engine/engine-paper/src/main/kotlin/com/typewritermc/engine/paper/utils/ThreadType.kt
@@ -3,16 +3,9 @@ package com.typewritermc.engine.paper.utils
 import com.github.shynixn.mccoroutine.bukkit.asyncDispatcher
 import com.github.shynixn.mccoroutine.bukkit.launch
 import com.github.shynixn.mccoroutine.bukkit.minecraftDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import lirand.api.extensions.server.server
 import com.typewritermc.engine.paper.plugin
-import kotlinx.coroutines.CloseableCoroutineDispatcher
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.newFixedThreadPoolContext
+import kotlinx.coroutines.*
+import lirand.api.extensions.server.server
 
 enum class ThreadType {
     SYNC,
@@ -54,7 +47,7 @@ enum class ThreadType {
         return plugin.launch(
             when (this) {
                 SYNC -> plugin.minecraftDispatcher
-                ASYNC -> plugin.minecraftDispatcher
+                ASYNC -> plugin.asyncDispatcher
                 DISPATCHERS_ASYNC -> pool ?: Dispatchers.IO
                 REMAIN -> if (server.isPrimaryThread) plugin.minecraftDispatcher else plugin.asyncDispatcher
             }
