45ee7421432740e15d94e5b76ce527815f5cc2bb
Marten-Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2025-11-08 14:05:21 +0100

    [Quest] Improve CachableFactObjective display

--- extensions/QuestExtension/src/main/kotlin/com/typewritermc/quest/entries/interfaces/CachableFactObjective.kt
+++ extensions/QuestExtension/src/main/kotlin/com/typewritermc/quest/entries/interfaces/CachableFactObjective.kt
@@ -1,18 +1,31 @@
 package com.typewritermc.quest.entries.interfaces
 
 import com.typewritermc.core.entries.Ref
+import com.typewritermc.core.entries.emptyRef
 import com.typewritermc.core.extension.annotations.Colored
 import com.typewritermc.core.extension.annotations.Help
+import com.typewritermc.core.extension.annotations.Negative
 import com.typewritermc.core.extension.annotations.Placeholder
-import com.typewritermc.engine.paper.entry.PlaceholderParser
+import com.typewritermc.core.interaction.InteractionContext
+import com.typewritermc.engine.paper.entry.*
 import com.typewritermc.engine.paper.entry.entries.CachableFactEntry
+import com.typewritermc.engine.paper.entry.entries.ConstVar
 import com.typewritermc.engine.paper.entry.entries.Var
-import com.typewritermc.engine.paper.entry.include
-import com.typewritermc.engine.paper.entry.literal
-import com.typewritermc.engine.paper.entry.placeholderParser
-import com.typewritermc.engine.paper.entry.supplyPlayer
+import com.typewritermc.engine.paper.entry.entries.get
+import com.typewritermc.engine.paper.extensions.placeholderapi.parsePlaceholders
+import com.typewritermc.engine.paper.facts.FactData
+import com.typewritermc.engine.paper.snippets.snippet
+import com.typewritermc.engine.paper.utils.replaceTagPlaceholders
 import com.typewritermc.quest.entries.ObjectiveEntry
+import com.typewritermc.quest.entries.inactiveObjectiveDisplay
+import com.typewritermc.quest.entries.showingObjectiveDisplay
 import org.bukkit.entity.Player
+import kotlin.math.absoluteValue
+
+val objectiveDisplay by snippet(
+    "quest.objectives.cachable.completed",
+    "<green>✔</green> <gray><display></gray>"
+)
 
 interface CachableFactObjective : ObjectiveEntry {
 
@@ -47,4 +60,26 @@ interface CachableFactObjective : ObjectiveEntry {
             }
         }
     }
+
+    override fun display(player: Player?): String {
+        val text = when {
+            player == null -> inactiveObjectiveDisplay
+            progressTracking.operator.isValid(
+                progressTracking.value.get()?.readForPlayersGroup(player)?.value ?: 0,
+                progressTracking.target.get(player).absoluteValue
+            ) -> objectiveDisplay
+
+            criteria.matches(player) -> showingObjectiveDisplay
+            else -> inactiveObjectiveDisplay
+        }
+        return text
+            .replaceTagPlaceholders("display", display.get(player) ?: "")
+            .replaceTagPlaceholders(
+                "value",
+                progressTracking.value.get()?.readForPlayersGroup(player!!)?.value?.toString() ?: "0"
+            )
+            .replaceTagPlaceholders("target", progressTracking.target.get(player).toString())
+            .parsePlaceholders(player)
+    }
+}
 }
