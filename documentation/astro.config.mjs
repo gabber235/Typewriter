fe2af7e6e08cbbb6395ca13d86d22a74c54bac63
Marten-Mrfc <martensog@gmail.com>
2026-01-26 20:39:04 +0100

    [Docs] Let's just search!

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 import tailwindcss from "@tailwindcss/vite";
+import starlightDocSearch from "@astrojs/starlight-docsearch";
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,7 +24,7 @@ export default defineConfig({
       customCss: ["./src/styles/global.css"],
       components: {
         Sidebar: "./src/components/sidebar/Sidebar.astro",
-		    PageFrame: "./src/components/pageframe/PageFrame.astro",
+        PageFrame: "./src/components/pageframe/PageFrame.astro",
         Header: "./src/components/header/Header.astro",
         SiteTitle: "./src/components/header/SiteTitle.astro",
         ThemeSelect: "./src/components/header/ThemeSelect.astro",
@@ -32,6 +33,13 @@ export default defineConfig({
   ],
 
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      starlightDocSearch({
+        appId: "GE6F02MN59",
+        apiKey: "57ae467d6c3f66ac2cae2c98e4275f49",
+        indexName: "typewriter",
+      }),
+    ],
   },
 });
