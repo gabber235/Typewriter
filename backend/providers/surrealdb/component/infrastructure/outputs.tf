e5cffe1aecadd802b7dc90444779c395957a0ed0
Gabber235 <gabber200@icloud.com>
2026-07-01 13:36:43 +0200

    refactor(backend): remove surrealdb provider

--- backend/providers/surrealdb/component/infrastructure/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "workload_name" {
-  description = "Name of the deployed WorkloadDeployment"
-  value       = "surrealdb-test-component"
-}
-
-output "namespace" {
-  description = "Namespace where surrealdb-test-component is deployed"
-  value       = local.wasmcloud_namespace
-}
--- backend/providers/surrealdb/surrealdb-component/src/types/mod.rs
+++
@@ -1,5 +0,0 @@
-mod datetime;
-mod record_id;
-
-pub use datetime::Datetime;
-pub use record_id::{RecordId, RecordIdKey};
