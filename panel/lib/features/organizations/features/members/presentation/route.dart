49df8baf5241330f96e2e271cfe1cf3a57826485
Gabber235 <gabber200@icloud.com>
2026-07-19 09:32:03 +0200

    style(panel): expand members route build

--- panel/lib/features/organizations/features/members/presentation/route.dart
+++ panel/lib/features/organizations/features/members/presentation/route.dart
@@ -5,5 +5,7 @@ import "package:flutter/material.dart";
 class MembersPage extends StatelessWidget {
   const MembersPage({super.key});
   @override
-  Widget build(BuildContext context) => const AutoRouter();
+  Widget build(BuildContext context) {
+    return const AutoRouter();
+  }
 }
