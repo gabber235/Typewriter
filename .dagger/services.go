2b1b6528c0f5a182c18e188522cbcbab58250ad6
Gabber235 <gabber200@icloud.com>
2026-08-19 03:19:14 +0200

    build(services): aggregate all service checks

--- .dagger/services.go
+++ .dagger/services.go
@@ -2,15 +2,6 @@ package main
 
 import "dagger/typewriter/internal/dagger"
 
-var serviceGradleRoots = []string{
-	"libs/service-utils",
-	"libs/service-telemetry",
-	"libs/service-http",
-	"libs/service-communicator",
-	"libs/service-registrar",
-	"realm",
-}
-
 func (m *Typewriter) servicesGradleContainer(source *dagger.Workspace) *dagger.Container {
 	services := source.Directory("/services", dagger.WorkspaceDirectoryOpts{Gitignore: true})
 
@@ -23,11 +14,7 @@ func (m *Typewriter) servicesGradleContainer(source *dagger.Workspace) *dagger.C
 
 // +check
 func (m *Typewriter) ServicesCheck(source *dagger.Workspace) *dagger.Container {
-	container := m.servicesGradleContainer(source)
-	for _, root := range serviceGradleRoots {
-		container = container.
-			WithWorkdir("/workspace/services/" + root).
-			WithExec([]string{"./gradlew", "check", "--no-daemon"})
-	}
-	return container
+	return m.servicesGradleContainer(source).
+		WithWorkdir("/workspace/services").
+		WithExec([]string{"./build-logic/gradlew", "-p", ".", "check", "ktlintCheck", "--no-daemon"})
 }
